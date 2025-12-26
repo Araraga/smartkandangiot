3ba31730d9631acd5ab4886a506e769d6ea83cbc
araraga <argaartanta920@gmail.com>
2025-12-26 12:51:14 +0700

    fix gemini version

--- controllers/ai_controller.js
+++ controllers/ai_controller.js
@@ -2,7 +2,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const pool = require("../config/db");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 exports.chatWithAssistant = async (req, res) => {
   const { message, device_id } = req.body;
